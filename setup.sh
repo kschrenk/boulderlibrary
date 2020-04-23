3399cf105865c5bc70c5d9c7b9f47733f6642a67
Kilian <kilian.screnk@sz.de>
2020-04-23 08:49:57 +0200

    added gunicorn

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # Run in development mode
-export FLASK_APP=app.py
-export FLASK_DEBUG=True
-export FLASK_ENV=development
+# export FLASK_APP=app.py
+# export FLASK_DEBUG=True
+# export FLASK_ENV=development
 
 
 # environment variables for local dev
